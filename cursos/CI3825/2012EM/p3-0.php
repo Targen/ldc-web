9fea585c00e3971a2fdcbd38c75b8f55790d15ef
Manuel Gómez <targen@gmail.com>
2012-03-25 14:45:55 -0430

    Consulta de Magy

--- cursos/CI3825/2012EM/p3-0.php
+++ cursos/CI3825/2012EM/p3-0.php
@@ -30,6 +30,7 @@
                         <li><a href="#p11">Posibilidad de prórroga                                                                </a></li>
                         <li><a href="#p12">Directorio de trabajo y resolución de caminos de GCC                                   </a></li>
                         <li><a href="#p13">Uso de shell scripting para implementar el proyecto                                    </a></li>
+                        <li><a href="#p14">Uso no estructurado de <code>fork</code>                                               </a></li>
                 </ol>
                 <ol>
                         <li id="p1">
@@ -291,6 +292,77 @@ clean:
                                         <li><p>En efecto. Es válido que usen shell scripts para cualquier tarea de automatización de generación de código o compilación (aunque debe poder compilarse ejecutando <code>make</code> en el directorio de su proyecto), pero el propio código del proyecto debe terminar reduciéndose a archivos en el lenguaje de programación C. Además, el enunciado especifica ciertas restricciones sobre la implementación: debe usarse <code>fork</code> y <code>exec</code> en algunas partes para hacer ciertas cosas. Es decir que no se vale que su programa consista de <code>if (fork() == 0) execl("/bin/bash", "bash", "-c", "</code><em>un script que resuelva todo</em><code>", NULL);</code>.</p></li>
                                         <li><p>Este proyecto es notablemente sencillo si se hace con un shell script, así que es natural que quieran resolverlo al menos parcialmente con esas herramientas que son las más adecuadas para el problema. Eso, junto a las razones para no usar Make recursivamente que se detallan en el paper referido en el enunciado, hace que este proyecto no sea software muy bien diseñado para uso real. Sus objetivos son pedagógicos y no prácticos: la idea es hacer algo interesante que les enseñe a trabajar con árboles de procesos y jerarquías de archivos usando C, solidificar su entendimiento de Make e introducir el concepto de programas que generan programas, que es fundamental para la carrera.</p></li>
                                         <li><p>Hacer este proyecto en C no tiene sentido práctico real, pero es un buen ejercicio.</p></li>
+                                </ol>
+                        </li>
+                        <li id="p14">
+                                <h3>Uso no estructurado de <code>fork</code></h3>
+                                <h4>Pregunta</h4>
+                                <blockquote>
+                                        <ol>
+                                                <li><p>Mira, si no hago el fork, el programa me dice cuales son los archivos del nivel en el que estoy y me dice cuales son los directorios. Si hago el fork, me da la misma información, repitiéndola muchas veces y después se queda pegado.</p></li>
+                                                <li><blockquote>
+<pre><code><![CDATA[
+if(S_ISDIR(statbuf.st_mode)){
+    
+   pidhijo = fork();
+ 
+     if (pidhijo < 0) {
+  printf("Fork\n");
+  perror("fork");
+  exit(1);
+     }
+     
+     if (pidhijo == 0){
+printf("Directorio %s\n", currentPath); (Esto es lo único que no comento en la salida "buena")
+     //dircheck(dip, dit, statbuf, currentPath, depth, argv); (esta es la
+función recursiva)
+     }
+}
+]]></code></pre>
+                                                </blockquote></li>
+                                                <li><p>Qué estoy haciendo mal? T_T</p></li>
+                                        </ol>
+                                </blockquote>
+                                <h4>Respuesta</h4>
+                                <ol>
+                                        <li><p>Cuando ejecutan <code>fork</code>, el proceso se duplica y continúa su ejecución en exactamente el mismo código en exactamente el mismo lugar, con (casi) exactamente los mismos descriptores de archivo y (casi) el mismo entorno. Las diferencias exactas entre el padre y el hijo están enumeradas en la página de manual de <code>fork</code>, pero la que les interesa es básicamente que el retorno de <code>fork</code> es diferente (0 en el hijo, y el PID del hijo en el padre).</p></li>
+                                        <li><p>Lo típico que se hace luego de hacer <code>fork</code> es obligar a que el hijo se vaya por un camino de ejecución totalmente diferente al del padre y que nunca vayan a terminar ejecutando las mismas cosas. En esencia es algo así:</p></li>
+                                        <li><blockquote>
+<pre><code><![CDATA[
+/* código del padre */
+switch (fork()) {
+    case -1: perror("fork"); exit(EX_OSERR);
+    case 0: /* código del hijo */ exit(EX_OK);
+    default: break;
+}
+/* el resto del código del padre */
+]]></code></pre>
+                                        </blockquote></li>
+                                        <li><p>Lo más importante es el segundo <code>exit</code>: el hijo nunca debería salir de ahí y continuar su flujo de ejecución por donde pasó el padre.</p></li>
+                                        <li><p>Esto no es una regla universal en el sentido de que sea necesario para que las cosas funcionen, pero es extremadamente difícil escribir código que funcione bien y no trabaje así. Por ejemplo, este es un programa en C que imprime los números del 1 al 1000 en un orden no determininístico usando forks no estructurados:</p></li>
+                                        <li><blockquote>
+<pre><code><![CDATA[
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 0, base = 0, incr = 10;
+    while (i < 10) {
+        switch (incr > 0 ? fork() : 1) {
+            case -1: perror("fork"); exit(EXIT_FAILURE);
+            case 0: incr /= 10; i = 0; break;
+            default:
+                if (incr) ++i; else { printf("%d\n", base); i = 10; }
+                base += incr; break;
+        }
+    }
+}
+]]></code></pre>
+                                        </blockquote></li>
+                                        <li><p>Analizar un programa así es sumamente difícil, y es muy fácil equivocarse y hacer que el programa haga <code>fork</code> infinitamente produciendo algo llamado <em>fork bomb</em>: el programa no para de duplicarse y se satura la tabla de procesos del sistema, lo que dificulta ejecutar cualquier programa, como <code>kill</code>, para terminar su ejecución (y el sistema deja de funcionar).</p></li>
+                                        <li><p>La mejor manera de evitar estos problemas es nunca escribir un <code>fork</code> no estructurado: el código del hijo debería ejecutarlo únicamente el hijo, y el código del padre debería ejecutarlo únicamente el padre.</p></li>
+                                        <li><p>Claro, hay muchos problemas donde el hijo debe ejecutar lo mismo que el padre con otros parámetros: cualquier función recursiva podría ejecutarse con una sola invocación por proceso haciendo las invocaciones recursivas en hijos. Este proyecto tiene una estructura recursiva que hace que esta técnica sea muy natural. Pero en cualquier caso habría una frontera bien definida entre el código del padre y el código del hijo: el hijo solo ejecuta la llamada recursiva y su ejecución termina al salir de la función, o la función nunca retorna.</p></li>
                                 </ol>
                         </li>
                 </ol>
